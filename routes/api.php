fb767ad6b7dfea19565796c3a92a82bf7985b80d
Amohelang Thoabala <amohelangt@gmail.com>
2024-08-12 14:24:52 +0200

    Add get all resaurants API

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RestaurantController;
 
+Route::get('/restaurants', [RestaurantController::class, 'index']); // Get all restaurants
+// Route::post('/restaurants', [RestaurantController::class, 'store']); // Create a new restaurant
+// Route::get('/restaurants/{id}', [RestaurantController::class, 'show']); // Get a specific restaurant
+// Route::put('/restaurants/{id}', [RestaurantController::class, 'update']); // Update a restaurant
+Route::delete('/restaurants/{id}', [RestaurantController::class, 'destroy']);
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
