1ffa0ca93180edd16e5cf5b8a09222ea0abec5a3
Amohelang Thoabala <amohelangt@gmail.com>
2024-08-26 15:06:29 +0200

    Procfile for Heroku

--- database/migrations/2024_06_18_181749_create_addresses_table.php
+++ database/migrations/2024_06_18_181749_create_addresses_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('addresses', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')->nullable()->constrained();
             $table->string('street');
             $table->string('city');
             $table->string('district');
