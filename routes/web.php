4185104e13ed73298ab6cbcef020b3579f8a5dc1
Lereko Maluke <lerekomaduke@gmail.com>
2024-11-20 15:46:30 +0200

    Home, explore, cart and Meal detail template

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('/explore', 'explore')->name('explore');
+
+Route::view('/cart', 'cart')->name('cart');
+
+Route::view('/details', 'meal')->name('details');
+
 Route::get('/docs', function () {
     return view('venodr.index');
 });
